7c2a0c21b6bc987a3c7f5ba624d2c813a29e9dc5
SooLee <duplexa@gmail.com>
2016-11-04 16:52:27 -0400

    format change for downloads.sh

--- downloads.sh
+++ downloads.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-## SOFTWARE: bwa-0.7.15-1142-dirty
+## SOFTWARE: bwa
+## VERSION: 0.7.15-1142-dirty
+## COMMIT: 5961611c358e480110793bbf241523a3cfac049
 ## This version of bwa is used because it has the -5 option.
 git clone https://github.com/lh3/bwa
 cd bwa
@@ -11,7 +13,8 @@ mv bwa bwa-0.7.15-1142-dirty
 ln -s bwa-0.7.15-1142-dirty bwa
 
 
-## SOFTWARE: samtools-1.2
+## SOFTWARE: samtools
+## VERSION: 1.2
 wget https://github.com/samtools/samtools/releases/download/1.2/samtools-1.2.tar.bz2
 tar -xjf samtools-1.2.tar.bz2
 cd samtools-1.2
